59f6c7ac7c917202cb4938e90d24c845e7fabd54
Will Constable <willconstable@gmail.com>
2015-05-10 12:01:17 -0700

    wip untested but running extract rgb kernel

--- Problem Sets/Problem Set 6/student_func.cu
+++ Problem Sets/Problem Set 6/student_func.cu
@@ -61,6 +61,11 @@
 
     In this assignment we will do 800 iterations.
    */
+#define NBUF 2
+#define NCHAN 3
+#define ch_R 0
+#define ch_G 1
+#define ch_B 2
 
 #define NON_WHITE x
 #define INTERIOR y
@@ -101,6 +106,18 @@ __global__ void mask_kern2(uchar4* sourceImg, uchar4* masks, int numRowsSource, 
   }
 }
 
+__global__ void extract_rgb_kern(uchar4* sourceImg, float* r, float* g, float* b, int numRowsSource, int numColsSource)
+{
+
+  int  gTid = ( blockIdx.x * blockDim.x ) + threadIdx.x;
+  size_t numElem = numRowsSource * numColsSource;
+
+  if(gTid < numElem){
+    r[gTid] = (float)sourceImg[gTid].x;
+    g[gTid] = (float)sourceImg[gTid].y;
+    b[gTid] = (float)sourceImg[gTid].z;
+  }
+}
 void your_blend(const uchar4* const h_sourceImg,  //IN
                 const size_t numRowsSource, const size_t numColsSource,
                 const uchar4* const h_destImg, //IN
@@ -109,25 +126,13 @@ void your_blend(const uchar4* const h_sourceImg,  //IN
 
   /* To Recap here are the steps you need to implement
 
-     mask buffer
-     interior buffer
-     input 2x R, G, B float bufs for guesses
-
-
      1) Compute a mask of the pixels from the source image to be copied
         The pixels that shouldn't be copied are completely white, they
         have R=255, G=255, B=255.  Any other pixels SHOULD be copied.
 
-        > alloc 1 uchar4
-        > cp uchar4 from htod
-
-        > compute masks
-          -nonwhite mask != 255 (one mask, for r,g and b)
-          -interior mask
-            all 4 neighbors also in mask
-          -border mask
-            remaining pixels in nonwhite mask that aren't interior
-
+     2) Compute the interior and border regions of the mask.  An interior
+        pixel has all 4 neighbors also inside the mask.  A border pixel is
+        in the mask itself, but has at least one neighbor that isn't.
 
         */
   size_t numElem = numRowsSource * numColsSource;
@@ -142,11 +147,14 @@ void your_blend(const uchar4* const h_sourceImg,  //IN
   const dim3 blockSize(256, 1, 1);
   const dim3 gridSize( (numElem + 1) / blockSize.x, 1, 1);
   mask_kern<<<gridSize, blockSize>>>(d_sourceImg, d_masks, numRowsSource, numColsSource);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
   mask_kern2<<<gridSize, blockSize>>>(d_sourceImg, d_masks, numRowsSource, numColsSource);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
 
   // debug
-  cudaMemcpy(h_blendedImg, d_masks, sizeof(uchar4) * numElem, cudaMemcpyDeviceToHost);
+//  cudaMemcpy(h_blendedImg, d_masks, sizeof(uchar4) * numElem, cudaMemcpyDeviceToHost);
 
   /*
         > alloc 3 (r,g,b) device float bufs x2 for guesses
@@ -156,16 +164,28 @@ void your_blend(const uchar4* const h_sourceImg,  //IN
           -- cast fp vals to uchar since they have been clamped already (in jacobi?)
 
 
-     2) Compute the interior and border regions of the mask.  An interior
-        pixel has all 4 neighbors also inside the mask.  A border pixel is
-        in the mask itself, but has at least one neighbor that isn't.
-
      3) Separate out the incoming image into three separate channels
 
      4) Create two float(!) buffers for each color channel that will
         act as our guesses.  Initialize them to the respective color
         channel of the source image since that will act as our intial guess.
 
+*/
+
+  float* d_jacBuf[NBUF][NCHAN];
+  for(int n = 0; n < NBUF; n++){
+    for(int c = 0; c < NCHAN; c++){
+      checkCudaErrors(cudaMalloc(&d_jacBuf[n][c],  sizeof(float) * numElem));
+    }
+  }
+
+  extract_rgb_kern<<<gridSize, blockSize>>>(d_sourceImg, d_jacBuf[0][ch_R], d_jacBuf[0][ch_G], d_jacBuf[0][ch_B], numRowsSource, numColsSource);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  extract_rgb_kern<<<gridSize, blockSize>>>(d_sourceImg, d_jacBuf[1][ch_R], d_jacBuf[1][ch_G], d_jacBuf[1][ch_B], numRowsSource, numColsSource);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  /*
      5) For each color channel perform the Jacobi iteration described
         above 800 times.
 
