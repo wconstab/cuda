27bb2b62f6aa7c796603d025bc8ec1028dcc0cb6
Will Constable <willconstable@gmail.com>
2020-02-11 02:02:36 +0000

    WIP stuck with some kind of memory access violation

--- matmul/matmul.cu
+++ matmul/matmul.cu
@@ -1,3 +1,4 @@
+
 #include <functional>
 #include <stdio.h>
 #include "mkl.h"
@@ -15,9 +16,49 @@ typedef struct {
 
 // Forward declaration of the matrix multiplication kernel
 __global__ void MatMulKernel(const Matrix, const Matrix, Matrix);
+__global__ void BlockMatMulKernel(const Matrix, const Matrix, Matrix, int sh_A_offs, int sh_B_offs);
 
-// Matrix multiplication - Host code
-// Matrix dimensions are assumed to be multiples of BLOCK_SIZE
+void BlockMatMul(const Matrix A, const Matrix B, Matrix C)
+{
+    // Load A and B to device memory
+    Matrix d_A;
+    d_A.width = A.width; d_A.height = A.height;
+    size_t size = A.width * A.height * sizeof(float);
+    cudaMalloc(&d_A.elements, size);
+    cudaMemcpy(d_A.elements, A.elements, size,
+               cudaMemcpyHostToDevice);
+    Matrix d_B;
+    d_B.width = B.width; d_B.height = B.height;
+    size = B.width * B.height * sizeof(float);
+    cudaMalloc(&d_B.elements, size);
+    cudaMemcpy(d_B.elements, B.elements, size,
+               cudaMemcpyHostToDevice);
+
+    // Allocate C in device memory
+    Matrix d_C;
+    d_C.width = C.width; d_C.height = C.height;
+    size = C.width * C.height * sizeof(float);
+    cudaMalloc(&d_C.elements, size);
+
+    // Invoke kernel
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid(B.width / dimBlock.x, A.height / dimBlock.y);
+    int shared = 2 * A.width * dimBlock.y * sizeof(float);
+    int sh_A_offs = 0;
+    int sh_B_offs = A.width * dimBlock.y;
+    printf("shared %d b_offs %d\n", shared, sh_B_offs);
+    BlockMatMulKernel<<<dimGrid, dimBlock, shared>>>(d_A, d_B, d_C, sh_A_offs, sh_B_offs);
+
+    // Read C from device memory
+    cudaMemcpy(C.elements, d_C.elements, size,
+               cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(d_A.elements);
+    cudaFree(d_B.elements);
+    cudaFree(d_C.elements);
+}
+
 void MatMul(const Matrix A, const Matrix B, Matrix C)
 {
     // Load A and B to device memory
@@ -55,6 +96,65 @@ void MatMul(const Matrix A, const Matrix B, Matrix C)
     cudaFree(d_C.elements);
 }
 
+/**
+    Cooperate to load relevant slice of A, B into shmem before multiplying
+*/
+__global__ void BlockMatMulKernel(Matrix A, Matrix B, Matrix C, int sh_A_offs, int sh_B_offs)
+{
+    extern __shared__ float s[];
+    float* shared_A = &s[sh_A_offs];
+    float* shared_B = &s[sh_B_offs];
+
+    if(blockIdx.x == 0 and blockIdx.y == 0 and threadIdx.x == 0 and threadIdx.y == 0)
+    {
+        printf("shared_a 0x%p shared_b 0x%p\n", shared_A, shared_B);
+    }
+
+    int max_idx = blockDim.x * blockDim.y;
+    int a_elts = A.width * A.height;
+    int iters = a_elts / max_idx;
+    int idx;
+    int my_pos = threadIdx.x + threadIdx.y * blockDim.x;
+    for(int i = 0; i < iters; i++)
+    {
+        idx = my_pos + i * max_idx;
+        if(idx < a_elts)
+        {
+            shared_A[idx] = A.elements[idx];
+        }
+    }
+    __syncthreads();
+
+    int sh_idx;
+    for(int iter = 0; iter < iters; iter++)
+    {
+        idx = (iter * blockDim.y * B.width)
+            + (threadIdx.y * B.width)
+            + threadIdx.x;
+        sh_idx = ((blockDim.x - 1 - threadIdx.x) * B.height)
+               + (iter * blockDim.y)
+               + threadIdx.y;
+        //if(blockIdx.x == 0 and blockIdx.y == 0) 
+        //{
+        //    printf("shared_B[%d] = B.elements[%d]\n", sh_idx, idx);
+        //    shared_B[sh_idx] = B.elements[idx];
+        //}
+        shared_B[sh_idx] = 0;
+    }
+    __syncthreads();
+
+
+    // Each thread computes one element of C
+    // by accumulating results into Cvalue
+    float Cvalue = 0;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int e = 0; e < A.width; ++e)
+        Cvalue += shared_A[row * A.width + e]
+                * B.elements[e * B.width + col];
+//                * shared_B[row * A.width + e];
+    C.elements[row * C.width + col] = Cvalue;
+}
 // Matrix multiplication kernel called by MatMul()
 __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C)
 {
@@ -134,7 +234,7 @@ int main(int argc, char ** argv)
 	init_mat(A, init_A);
 	init_mat(B, init_B);
 	init_mat(C, init_zeros);
-    MatMul(A, B, C);
+    BlockMatMul(A, B, C);
 
     Matrix ref_C;
     ref_C.height = C.height;
